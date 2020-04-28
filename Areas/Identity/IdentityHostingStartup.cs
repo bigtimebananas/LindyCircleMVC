b9a290d49eaa736e51f73668869369b5d1352203
mpipkin <michael.a.pipkin@gmail.com>
2020-04-28 15:13:03 -0400

    Misc

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using LindyCircleMVC.Models;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(LindyCircleMVC.Areas.Identity.IdentityHostingStartup))]
 namespace LindyCircleMVC.Areas.Identity
